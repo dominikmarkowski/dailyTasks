2423cf66f94ce81e42564b88c0fb0e6f5e3035f7
dominikmarkowski <dominik.markowski95@gmail.com>
2016-05-02 02:41:23 +0200

    deleted angular's error and useless files

--- app/components/version/version_test.js
+++ app/components/version/version_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-describe('myApp.version module', function() {
-  beforeEach(module('myApp.version'));
+describe('dailyTasksApp.version module', function() {
+  beforeEach(module('dailyTasksApp.version'));
 
   describe('version service', function() {
     it('should return current version', inject(function(version) {
